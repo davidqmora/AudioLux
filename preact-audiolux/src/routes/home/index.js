b2dad29a06c37771e54f6f42e8ad3af62076b22e
Andrew Yemtsev <59855700+yemtseva@users.noreply.github.com>
2022-11-28 15:59:59 -0500

    Add files via upload

--- preact-audiolux/src/routes/home/index.js
+++ preact-audiolux/src/routes/home/index.js
@@ -4,7 +4,10 @@ import style from './style.css';
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<h4 style="color:purple;">Audiolux: A Modern “Visual Sound” System for Deaf & Hard-of-Hearing</h4>
+		<img src="https://www.cymaspace.org/wp-content/uploads/2015/11/Audiolux-One-System.jpg"  width="500" height="333"/>
+		<p>AUDIOLUX is an open source digital lighting system that allows the Deaf & Hard-of-Hearing to see music & alerts using the Arduino hardware/software platform & modern digital individually addressable LEDs. Source code is provided under open-source licensing for anyone interested to develop their own system.
+		</p>
 	</div>
 );
 
